80e93dfaa68796ff4f5ccfcfd44a96b4057b1b67
Nia292 <73426965+Nia292@users.noreply.github.com>
2023-06-18 17:32:17 +0200

    More data

--- src/util/icon.ts
+++ src/util/icon.ts
@@ -9,9 +9,15 @@ export function determineIcon(location: MapLocation): string {
         case LocationGroupType.RESOURCE_ORE:
             return 'icon_mineral.png';
         case LocationGroupType.POINT_OF_INTEREST:
-            return 'icon_vista.png'
+            return 'icon_vista.png';
         case LocationGroupType.RESOURCE_PLANT:
-            return 'icon_plant.png'
+            return 'icon_plant.png';
+        case LocationGroupType.RUIN:
+            return 'icon_ruin.png';
+        case LocationGroupType.DUNGEON:
+            return 'icon_vault.png';
+        case LocationGroupType.THRALL_CAMP_BLACK_HAND:
+            return 'icon_black_hand.png';
     }
     console.error('Failed to determine icon for ', location)
     return '';
